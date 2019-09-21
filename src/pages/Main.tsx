e82153ddc532e22ec3abd44a46d6cca65481507b
ChoSeoHwan <wtae1216@naver.com>
2019-09-22 01:23:23 +0900

    기본 세팅  - 불필요한 파일 제거  - tslint, prettier 적용  - directory structure 세팅  - yarn 명령어 추가   > lint-fix : tslint 오류난 파일 전체 검사 및 자동 수정  - 기타 라이브러리 설치   > react-router-dom   > styled-components

---
+++ src/pages/Main.tsx
@@ -0,0 +1,8 @@
+import React, { Fragment } from 'react';
+import { Route } from 'react-router';
+
+const Main: React.FC = () => {
+    return <Fragment />;
+};
+
+export default Main;
---
+++ src/styles/common.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Nanum+Gothic');
+  
+  * {
+    margin: 0;
+    user-select: none;
+  }
+  
+  html, body  {
+    color: #2a2a2a;
+    font-size: 14px;
+    font-family: 'Nanum Gothic', sans-serif;
+  }
+  
+  body  {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+  }
+  
+`;
+
+export default GlobalStyle;
